8d71b02a08d1f5d120bddad3c364eb78a5522333
a <ruan65@gmail.com>
2017-12-25 00:04:39 +0300

    add toolbar 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
-import Toolbar from './components/Navigation/Toolbar/Toolbar'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
   render() {
     return (
         <Layout>
-          <Toolbar/>
           <BurgerBuilder/>
         </Layout>
     );
